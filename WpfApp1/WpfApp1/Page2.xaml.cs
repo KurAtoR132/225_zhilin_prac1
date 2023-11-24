51fa0128e9d5809e563456b01212f6c463003538
KurAtoR132 <151126527+KurAtoR132@users.noreply.github.com>
2023-11-24 18:45:26 +0300

    допаолнили табл

--- WpfApp1/WpfApp1/Page2.xaml.cs
+++ WpfApp1/WpfApp1/Page2.xaml.cs
@@ -16,57 +16,32 @@ using System.Windows.Shapes;
 namespace WpfApp1
 {
     /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
+    /// Логика взаимодействия для Page2.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class Page2 : Page
     {
-        public MainWindow()
+        public Page2()
         {
             InitializeComponent();
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            Answer.Clear();
-            double a, V, S;
-            if (!double.TryParse(NumberA.Text, out a))
-            {
-                MessageBox.Show("ТЫ ВООБЩЕ УМНЫЙ?");
 
-            }
-            V = Math.Pow(a, 3);
-            S = 6 * Math.Pow(a, 2);
-            Answer.Text += $"V = {V}\nS={S}";
         }
 
         private void Button_Click_1(object sender, RoutedEventArgs e)
         {
-            MyFrame.Navigate(new Page2());
 
         }
 
         private void Button_Click_2(object sender, RoutedEventArgs e)
         {
-      
 
         }
 
         private void Button_Click_3(object sender, RoutedEventArgs e)
         {
-           
-          
-
-        }
-
-        private void Button_Click_4(object sender, RoutedEventArgs e)
-        {
-       
-
-        }
-
-        private void Button_Click_5(object sender, RoutedEventArgs e)
-        {
-           
 
         }
     }
